9f6ccc38d2c87ca2001642ca093596b27a3bd0a2
RangerMauve <RangerMauve@htomail.com>
2014-12-11 23:11:06 -0500

    Progress to making dreams come true

---
+++ index.js
@@ -0,0 +1,42 @@
+var domDelegate = require("dom-delegate-stream");
+var htmlPatcher = require("html-patcher-stream");
+var immutableState = require("immutable-state-stream");
+var mustache = require("mustache");
+var map = require("through2-map");
+var fs = require("fs");
+var objectPath = require("object-path");
+
+var template = fs.readFileSync("template.html", "utf8");
+
+var main = document.querySelector("main");
+
+var delegate = domDelegate(main);
+
+var pipeline = map({
+		objectMode: true
+	}, generate_state_update)
+	.pipe(immutableState())
+	.pipe(map(render))
+	.pipe(htmlPatcher(main, render({})));
+
+delegate.on("change", "input, textarea").pipe(pipeline);
+delegate.on("keyup", "input, textarea").pipe(pipeline);
+delegate.on("paste", "input, textarea").pipe(pipeline);
+
+function generate_state_update(event) {
+	var element = event.target;
+	var binding = element.dataset.binding;
+	var value = element.value;
+	console.log(element, binding);
+	var data = {};
+	objectPath.set(data, binding, value);
+	console.log("Generated data", data);
+	return data;
+}
+
+function render(data) {
+	console.log("Rendering", data);
+	var html = mustache.render(template, data);
+	console.log("Rendered", html);
+	return html;
+}
