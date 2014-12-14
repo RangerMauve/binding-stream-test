cb02c3935990b8cba1abe10ddc30b2088b034746
RangerMauve <RangerMauve@htomail.com>
2014-12-14 02:08:41 -0500

    Added support for more input types

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var immutableState = require("immutable-state-stream");
 var mustache = require("mustache");
 var map = require("through2-map").obj;
 var fs = require("fs");
-var objectPath = require("object-path");
+var streamCombiner = require("stream-combiner2");
+var makeProp = require("make-prop");
 
 var template = fs.readFileSync("template.html", "utf8");
 
@@ -12,13 +13,13 @@ var main = document.querySelector("main");
 
 var delegate = domDelegate(main);
 
-var pipeline = map(generate_state_update);
+var pipeline = streamCombiner(
+	map(generate_state_update),
+	immutableState(),
+	map(render)
+);
 
-pipeline
-	.pipe(immutableState())
-	.pipe(map(render))
-	.pipe(htmlPatcher(main, render({})));
-
+pipeline.pipe(htmlPatcher(main, render({})));
 
 delegate.on("change", "input, textarea").pipe(pipeline);
 delegate.on("keyup", "input, textarea").pipe(pipeline);
@@ -27,17 +28,22 @@ delegate.on("paste", "input, textarea").pipe(pipeline);
 function generate_state_update(event) {
 	var element = event.target;
 	var binding = element.dataset.binding;
-	var value = element.value;
-	console.log(element, binding);
-	var data = {};
-	objectPath.set(data, binding, value);
-	console.log("Generated data", data);
+	var value;
+
+	if (element.type !== "radio")
+		value = element.value;
+	else if (element.checked)
+		value = element.value;
+
+	if (element.type === "checked")
+		value = element.checked;
+	var data = makeProp(binding)(value);
+	console.log("Update:", data.inputs);
 	return data;
 }
 
 function render(data) {
-	console.log("Rendering", data);
+	console.log("Rendering:", data.inputs);
 	var html = mustache.render(template, data);
-	console.log("Rendered", html);
 	return html;
 }
